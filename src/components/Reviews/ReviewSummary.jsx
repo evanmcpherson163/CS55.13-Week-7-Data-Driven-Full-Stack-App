d5c0f52372c59cb3633b90293323a4669d777181
Evan McPherson <evanmcpherson98@yahoo.com>
2024-10-30 14:16:31 -0700

    added AI, and replaced service worker file contents

--- src/components/Reviews/ReviewSummary.jsx
+++ src/components/Reviews/ReviewSummary.jsx
@@ -2,13 +2,59 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 import { getReviewsByRestaurantId } from "@/src/lib/firebase/firestore.js";
 import { getAuthenticatedAppForUser } from "@/src/lib/firebase/serverApp";
 import { getFirestore } from "firebase/firestore";
+import { HarmBlockThreshold, HarmCategory } from "@google/generative-ai";
 
 export async function GeminiSummary({ restaurantId }) {
-  return (
-    <div className="restaurant__review_summary">
-      <p>TODO: summarize reviews</p>
-    </div>
+  const { firebaseServerApp } = await getAuthenticatedAppForUser();
+  const reviews = await getReviewsByRestaurantId(
+      getFirestore(firebaseServerApp),
+      restaurantId
   );
+
+  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+  
+  const genAISafety = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshhold: HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+  ];
+  
+  const model = genAI.getGenerativeModel(
+    {
+      model: "gemini-1.5-flash",
+      safetySettings: []
+    }
+  );
+
+  const reviewSeparator = "@";
+  const prompt = `
+      Based on the following restaurant reviews, 
+      where each review is separated by a '${reviewSeparator}' character, 
+      create a one-sentence summary of what people think of the restaurant. 
+
+      Here are the reviews: ${reviews.map(review => review.text).join(reviewSeparator)}
+  `;
+
+  try {
+      const result = await model.generateContent(prompt);
+      const response = await result.response;
+      const text = response.text();
+
+      return (
+          <div className="restaurant__review_summary">
+              <p>{text}</p>
+              <p>✨ Summarized with Gemini</p>
+          </div>
+      );
+  } catch (e) {
+      console.error(e);
+      return <p>Error contacting Gemini</p>;
+  }
 }
 
 export function GeminiSummarySkeleton() {
